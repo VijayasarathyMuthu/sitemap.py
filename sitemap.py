64b1cfadf52b5f1f9ea9b0f052fef824a2d0a5cc
VijayasarathyMuthu <sarathy1988@gmail.com>
2022-11-18 14:54:28 +1100

    Update sitemap.py

--- sitemap.py
+++ sitemap.py
@@ -1,42 +1,21 @@
-import requests
 import streamlit as st
-from bs4 import BeautifulSoup
 import pandas as pd
-import json
+import advertools as adv
 
 url = "https://screenrant.com/sitemap.xml"
 
 url = st.text_input ("Input the url of the sitemap of the website")
 
 
-r = requests.get(url, headers={"User-Agent": "XY"})
 
-with st.spinner(text="Fetching urls..."):
-    with requests.Session() as req:
-            r = requests.get(url, headers={"User-Agent": "XY"})
-            soup = BeautifulSoup(r.content, 'html.parser')
-            links = [item.text for item in soup.select("loc")]
-            df_final = []
-            #links = links[0:2]
-            for link in links:
-                    link = link.strip()
-                    res = requests.get(link, headers={"User-Agent": "XY"})
-                    soup = BeautifulSoup(res.content, 'html.parser')
-                    end = [item.text for item in soup.select("loc")]
-                    lastmod = [item.text for item in soup.select("lastmod")]
-                    zipped = dict(enumerate(zip(end, lastmod)))
-                    df = pd.DataFrame.from_dict(zipped).T.rename(columns={0:"url",1:"Last_modified"})
-                    df_final.append(df)
-            df_final = pd.concat(df_final).reset_index().drop(columns=["index"])
-            st.dataframe(df_final.sample(10))
+with st.spinner('Fetching the data...'):
+    sitemap = adv.sitemap_to_df(url, recursive=True )
 st.success('Done!')
 
 @st.cache
-def convert_df(df_final):
-     # IMPORTANT: Cache the conversion to prevent computation on every rerun
-     return df_final.to_csv().encode('utf-8')
-
-csv = convert_df(df_final)
+def convert_df(sitemap):
+        # IMPORTANT: Cache the conversion to prevent computation on every rerun
+        return sitemap.to_csv().encode('utf-8')
 
 st.download_button(
      label="Download data as CSV",
